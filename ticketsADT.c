3d23c3eae4a72b40ef95140ec6ee3914a17d42a3
joaquin pelufo <joaquinpelufo@Mac.fibertel.com.ar>
2024-12-09 13:33:27 -0300

    update hola

---
+++ ticketsADT.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+typedef struct tFines{
+    char * whichFine;
+    size_t numberFine;
+} tFines;
+
+typedef struct tInfractions{
+    tFines * vecFines;
+    size_t possibleFines;
+    size_t dimFines;
+    size_t infractionID;
+} tInfractions;
+
+typedef struct tAgencies{
+    struct tAgencies * tail;
+    char agencyName[100];
+    size_t maxMount;
+    size_t minMount;
+}
